537243f6512be722ade23fb884362d2767e02931
ngandung <gandungn@gmail.com>
2024-12-03 13:03:10 +0700

    vue route

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMember from './components/teams/TeamMembers.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/teams', component: TeamsList }, //our-domain.com/teams
+        { path: '/users', component: UsersList },
+        { path: '/teams/:teamId', component: TeamMember },
+       
+    ],
+    linkActiveClass: 'active'
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
